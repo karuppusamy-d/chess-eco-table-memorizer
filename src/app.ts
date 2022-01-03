791c848f3c545153b25b655352dbbca82c0ad031
Karuppusamy <d.karuppusamy@outlook.com>
2022-01-04 01:49:15 +0530

    Add cache-control header

--- src/app.ts
+++ src/app.ts
@@ -5,13 +5,18 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (_req, res) => {
+  // Cache Control: 3 minutes
+  res.set("Cache-Control", "public, max-age=180");
   res.json(data);
 });
 
 app.get("/:id", (req, res) => {
-  const filtered = data[req.params.id];
-  if (filtered) {
-    res.json(filtered);
+  // Cache Control: 3 minutes
+  res.set("Cache-Control", "public, max-age=180");
+
+  const filteredData = data[req.params.id];
+  if (filteredData) {
+    res.json(filteredData);
   } else {
     res.status(404).json({ error: "Not found" });
   }
