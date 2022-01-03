d63a9e6d8d4920a2e4b377fbddfe06c01515e48e
Karuppusamy <d.karuppusamy@outlook.com>
2022-01-04 01:51:33 +0530

    Add api to find next move

--- src/app.ts
+++ src/app.ts
@@ -22,6 +22,31 @@ app.get("/:id", (req, res) => {
   }
 });
 
+app.get("/:id/*", (req, res) => {
+  // Cache Control: 3 minutes
+  res.set("Cache-Control", "public, max-age=180");
+
+  // Parse the query string
+  const query = req.params[0].endsWith("/")
+    ? req.params[0]
+    : `${req.params[0]}/`;
+
+  // Filter the data
+  const filteredData = data[req.params.id].moves
+    .split(" ")
+    .filter((move) => !move.match(/^[0-9]+/))
+    .join("/");
+
+  // Find the next move
+  const nextMove = filteredData.startsWith(query)
+    ? filteredData.slice(query.length).split("/")[0]
+    : "";
+
+  // Return the result
+  if (nextMove) res.json({ next: nextMove });
+  else res.status(404).json({ error: "Not found" });
+});
+
 app.listen(port, () => {
   return console.log(`Server is listening at http://localhost:${port}`);
 });
